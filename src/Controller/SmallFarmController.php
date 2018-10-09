165bcc03588ec770f0983ed9e83242239b476bdb
przemekdab1993 <przemekdab1993@gmail.com>
2018-10-09 10:04:30 +0200

    v 0.1.2
    +++
    + system logowania
    + system rejestracji
    + widok na pole

--- src/Controller/SmallFarmController.php
+++ src/Controller/SmallFarmController.php
@@ -26,12 +26,12 @@ class SmallFarmController extends AbstractController
     /**
      * @Route("/SmallFarm", name="SmallFarm")
      */
-    public function index()
+    public function index(Connection $connection)
     {
 		$session = new Session;
 		if($session->has('loged') == true)
 		{
-			return $this->home();	
+			return $this->home($session, $connection);	
 		}
 		else
 		{
@@ -40,6 +40,21 @@ class SmallFarmController extends AbstractController
 				'the_log' => "Login"
 			]);
 		}
+    }
+    private function home($session, $connection)
+	{	
+		$id_user = $session->get('user_id');
+		$SQL = "SELECT * FROM fields_user WHERE user_ID = '$id_user'";
+		$res = $connection->query($SQL);
+		$res = $res->fetchAll();
+		
+		
+		$connection->close();
+        return $this->render('small_farm/game/home.html.twig', [
+            'the_title' => "Small Farm",
+			'the_log' => "Logout",
+			'the_res' => $res
+		]);
     }
 	
 	/**
@@ -77,6 +92,7 @@ class SmallFarmController extends AbstractController
 			return $collapse;
 		}
 	}
+	
 	/**
      * @Route("/SmallFarm/login_pass", name="login_pass", methods={"POST",})
      */
@@ -85,7 +101,7 @@ class SmallFarmController extends AbstractController
 		$user_name = $request->request->get('user_name');
 		$password = $request->request->get('password');
 		
-		$SQL = " SELECT user.id FROM user WHERE user.user = '$user_name' AND user.password = '$password' ";
+		$SQL = " SELECT user_ID FROM user_game WHERE user_name = '$user_name' AND user_password = '$password' ";
 		$ser = $connection->query($SQL);
 		
 		if($ser->rowCount() >= 1)
@@ -95,13 +111,11 @@ class SmallFarmController extends AbstractController
 			$session = new Session;
 			$session->set('loged', 'true');
 			$session->set('user_name', $user_name);
-			$session->set('id_user', $ser[0]['id']);
+			$session->set('user_id', $ser[0]['user_ID']);
 			
 			$connection->close();
-			return $this->render('small_farm/game/home.html.twig', [
-				'the_title' => "Small Farm",
-				'the_log' => "Logout"
-			]);
+			
+			return $this->index($connection);
 		}
 		else
 		{
@@ -111,33 +125,69 @@ class SmallFarmController extends AbstractController
 			]);
 		}
     }
-	
-	/**
-     * @Route("/SmallFarm/home", name="home", methods={"POST",})
-     */
-    public function home()
-	{
-        return $this->render('small_farm/game/home.html.twig', [
-            'the_title' => 'Jesteś zalogowany',
-			'the_log' => "Logout"
-		]);
-    }
-	/**
-     * @Route("/SmallFarm/inventory", name="inventory")
-     */
-    public function inventory()
-	{
-        return $this->render('small_farm/game/inventory.html.twig', [
-            'the_title' => 'Jesteś zalogowany',
-			'the_log' => "Logout"
-		]);
-    }
 	/**
      * @Route("/SmallFarm/registration", name="registration")
      */
     public function registration()
 	{
         return $this->render('small_farm/registration.html.twig', [
+            'the_title' => 'Rejestracja',
+			'the_log' => "Logout"
+		]);
+    }
+	/**
+     * @Route("/SmallFarm/adduser", name="adduser", methods={"POST",})
+     */
+    public function adduser(Request $request, Connection $connection)
+	{
+		$user_name = $request->request->get('user_name');
+		$password_1 = $request->request->get('password');
+		$email = $request->request->get('email');
+		
+		$connection->insert('user_game', ['user_name' => $user_name, 'user_password' => $password_1,
+											'email' => $email, 'action_punkts' => '15',
+											'lvl' => '1', 'experience' => '0',
+											'silver_coins' => '10', 'gold_coins' => '1', 
+											'premium_day' => '7'
+										]);
+		$this->setfield($connection, $user_name);
+		
+		$connection->close();
+        return $this->render('small_farm/alert.html.twig', [
+            'the_title' => 'Success',
+			'the_log' => "Login",
+			'the_alert' => "Zarejestrowałeś się"
+		]);
+    }
+	private function setfield($connection, $user_name)
+	{
+		$SQL = "SELECT user_ID FROM user_game WHERE user_name = '$user_name';";
+		$res = $connection->query($SQL);
+		
+		$res = $res->fetchAll();
+		$user_id = $res[0]['user_ID'];
+		
+		$maps = [];
+		for($i = 0; $i < 80; $i++)
+		{
+			$maps[$i] = 1;
+		}
+		
+		for($i = 0; $i < 80; $i++)
+		{
+			$connection->insert('fields_user', ['user_ID' => $user_id, 'krotka_ID' => $i, 'content_ID' => $maps[$i], 'counter' => 0]);
+		}
+		return;
+	}
+	
+	
+	
+	/**
+     * @Route("/SmallFarm/inventory", name="inventory")
+     */
+    public function inventory()
+	{
+        return $this->render('small_farm/game/inventory.html.twig', [
             'the_title' => 'Jesteś zalogowany',
 			'the_log' => "Logout"
 		]);
